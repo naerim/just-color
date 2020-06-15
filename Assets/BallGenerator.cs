07a1e431b7f1e57169e133a370796e78595f027c
naerim <naerim1119@gmail.com>
2020-06-16 01:23:34 +0900

    Add BallGenerator

---
+++ Assets/BallGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+    public GameObject ballPrefab; // 공 프리팹 선언
+    float span = 1.0f;
+    float delta = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // 공을 1초마다 한 개씩 생성
+        this.delta += Time.deltaTime;
+        if(this.delta > this.span)
+        {
+            this.delta = 0;
+            GameObject go = Instantiate(ballPrefab) as GameObject;
+            int px = Random.Range(-6, 7);
+            go.transform.position = new Vector3(px, 7, 0);
+
+        }
+    }
+}
