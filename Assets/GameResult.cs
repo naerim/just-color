5a9336f0dc4c5f7c96c8bebe206d564b85190490
naerim <naerim1119@gmail.com>
2020-06-19 04:00:00 +0900

    Add GameResult

---
+++ Assets/GameResult.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameResult : MonoBehaviour
+{
+    private int highScore;
+    public Text resultScore;
+    public Text bestScore;
+    public GameObject resultUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayerPrefs.HasKey("HighScore"))
+        {
+            highScore = PlayerPrefs.GetInt("HighScore");
+        }
+        else
+        {
+            highScore = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(TheEnd.over)
+        {
+            resultUI.SetActive(true);
+            int result = Mathf.FloorToInt(Timer.time);
+
+            if (highScore < result)
+            {
+                PlayerPrefs.SetInt("HighScore", result);
+            }
+
+            resultScore.text = "Result Score: " + result;
+            bestScore.text = "Best Score: " + highScore;
+        }
+    }
+
+    public void OnRetry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GoStartScene()
+    {
+
+    }
+}
